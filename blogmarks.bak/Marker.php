9b2fac8c32ac92b7f10aa3c115480ff7468da419
mbertier <mbertier>
2004-03-01 17:35:15 +0000

    Configuration des DataObjects.

--- blogmarks.bak/Marker.php
+++ blogmarks.bak/Marker.php
@@ -1,10 +1,29 @@
 <?php
 /** Déclaration de la classe BlogMarks_Marker
- * @version    $Id: Marker.php,v 1.1 2004/02/26 13:37:34 mbertier Exp $
+ * @version    $Id: Marker.php,v 1.2 2004/03/01 17:35:15 mbertier Exp $
  */
+
+# -- CONFIGURATION
+
+# Libs
+ini_set( 'include_path', ini_get('include_path') . ':' . '/home/mbertier/dev/PEAR_OVERLAY' );  
+
+# Dataobjects
+$config = parse_ini_file('config.ini',TRUE);
+foreach( $config as $class => $values ) {
+    $options =& PEAR::getStaticProperty( $class, 'options' );
+    $options = $values;
+}
+
+# -- INCLUDES
+require_once 'Element/Bm_Marks';
+
+# -- 
+define( ELEM_MARK, 'Bm_Marks' );
 
 /** Classe "métier". Effectue tous les traitements et opérations.
  * @package    BlogMarks
+ * @uses       Elements
  */
 class BlogMarks_Marker {
 
@@ -16,6 +35,19 @@ class BlogMarks_Marker {
 
     /** Constructeur. */
     function BlogMarks_Marker () {}
+
+    
+    /** Création d'un mark. 
+     * @param      array     $props      Un tableau de paramètres décrivant le mark.
+     * @returns    string    L'URI du mark créé.
+     */
+    function createMark( $props ) {
+        // WOW g super bien avancé 
+        $CLASSNAME = ELEM_MARK;
+        $m = new $CLASSNAME;
+
+        return $mark_uri;
+    }
     
     
 # ----------------------- #

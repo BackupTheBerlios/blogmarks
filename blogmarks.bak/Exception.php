2b5ecb2ccea687588295a5cf780f8196d8e65b93
mbertier <mbertier>
2004-03-03 16:40:18 +0000

    Import initial

---
+++ blogmarks.bak/Exception.php
@@ -0,0 +1,29 @@
+<?php
+/** Déclaration de la classe Blogmarks_Exception
+ * @version    $Id: Exception.php,v 1.1 2004/03/03 16:40:18 mbertier Exp $
+ */
+
+/** Objet dédié à la remontée d'erreurs
+ * @package    Blogmarks
+ */
+class Blogmarks_Exception {
+
+    var $_code;
+    var $_message;
+
+# ----------------------- #
+# -- METHODES PUBLIQUES --#
+# ----------------------- #
+
+    /** Constructeur. */
+    function Blogmarks_Exception () {}
+    
+    
+# ----------------------- #
+# -- METHODES PRIVEES   --#
+# ----------------------- #
+    
+    
+}
+?>
+

9c38e361d7552d04d9820d3da1a3cbcf54b3e32f
mbertier <mbertier>
2004-03-04 16:11:55 +0000

    Links::fetchUrlInfo est stable. Pour le moment, il ne remplit que le 1. du contrat (voir dans les commentaires)

--- blogmarks.bak/Element/Bm_Links.php
+++ blogmarks.bak/Element/Bm_Links.php
@@ -25,5 +25,56 @@ class Element_Bm_Links extends Blogmarks_Element
 
     /* the code above is auto generated do not remove the tag below */
     ###END_AUTOCODE
+
+
+    /** Récupèration d'informations à propos de la ressource désignée par $this->href.
+     *
+     * La récupération des infos se fait en cascade :
+     *   1. the encoding given in the charset parameter of the Content-Type HTTP header, or
+     *   2. the encoding given in the encoding attribute of the XML declaration within the document, or
+     *   3. utf-8.
+     *
+     * @see      http://diveintomark.org/archives/2004/02/13/xml-media-types
+     * @todo     Error testing
+     * @todo     Régénérer la classe (+champs charset)
+     * @todo     Moteur d'expressions régulières
+     */
+    function fetchUrlInfo() {
+
+        // Envoi d'une requete vers l'URL
+        require_once 'HTTP/Request.php';
+        $r = new HTTP_Request( $this->href );
+        $r->sendRequest();
+
+        // Récupération des informations contenues dans l'entête de la réponse.
+        $url_info = $r->getResponseHeader();
+
+        // --- Détermination du Content-Type et du Charset de la page --- //
+        // 1. D'apres un entete http
+        if ( isset($url_info['content-type']) ) {
+
+            $pattern = '/^(\w+\/\w+)(;?.\w+=(.*))?/';
+            if ( preg_match( $pattern, $url_info['content-type'], $matches ) ) {
+                $content_type = isset($matches[1]) ? $matches[1] : null;
+                $charset = isset($matches[3]) ? $matches[3] : null;
+            }
+
+        }
+
+        // 2. D'apres la déclaration XML du document
+        
+
+        // 3. Par défaut : utf8
+        else {
+            $charset = 'utf8';
+        }
+        // Mise à jour des propriétés de l'objet en fonction des informations obtenues
+        $this->type_2 = $content_type;
+        $this->charset = $charset;
+
+        return true;
+
+        
+    }
 }
 ?>

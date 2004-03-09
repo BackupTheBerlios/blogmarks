3e46f826c24c487697e04248c151af723c40afcf
mbertier <mbertier>
2004-03-09 16:56:43 +0000

    implémentation de la méthode populateProps

--- blogmarks.bak/Element.php
+++ blogmarks.bak/Element.php
@@ -1,6 +1,6 @@
 <?php
 /** Déclaration de la classe Blogmarks_Element
- * @version    $Id: Element.php,v 1.4 2004/03/05 10:21:05 mbertier Exp $
+ * @version    $Id: Element.php,v 1.5 2004/03/09 16:56:43 mbertier Exp $
  */
 
 include_once 'DB/DataObject.php';
@@ -16,7 +16,28 @@ class Blogmarks_Element extends DB_DataObject {
   
   /** Constructeur. */
   function BlogMarks_Element () {}
-  
+
+
+  /** Initialisation de propriétés en fonction d'un tableau associatif.
+   * Les valeurs des clés du tableau correspondant à une propriétés de l'objet
+   * sont utilisées pour la mise à jour. Les autres propriétés sont ignorées.
+   * 
+   * @param      array      $props
+   * @return
+   */
+  function populateProps( $props ) {
+
+      // Récupération des propriétés par défaut de l'objet
+      $my_props = get_class_vars( get_class($this) );
+
+      foreach ( $props as $label => $value ) {
+          // On ne modifie que les propriétés connues
+          if ( array_key_exists($label, $my_props) ) {
+              $this->$label = $value;
+          }
+      }
+
+  }
   
 # ----------------------- #
 # -- METHODES PRIVEES   --#

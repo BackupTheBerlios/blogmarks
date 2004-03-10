94b64ca72ee1c35bf08e25778dcf3f6b4fcef1b9
benfle <benfle>
2004-03-10 16:40:52 +0000

    Implémentation de l'architecture du serveur

--- blogmarks.bak/Server/Atom.php
+++ blogmarks.bak/Server/Atom.php
@@ -1,29 +1,43 @@
 <?php
 /** Déclaration de la classe Server_Atom
- * @version    $Id: Atom.php,v 1.1 2004/02/26 13:37:34 mbertier Exp $
+ * @version    $Id: Atom.php,v 1.2 2004/03/10 16:40:52 benfle Exp $
  */
 
-/** Description de la classe.
+require_once 'PEAR.php';
+
+/** Classe du serveur Atom de BlogMarks.
  * @package    Servers
  * @subpackage Atom
  */
 class Server_Atom {
 
+  /** Constructeur. */
+  function Server_Atom () {
+  }
 
+  /** Lance le serveur */
+  function run () {
 
-# ----------------------- #
-# -- METHODES PUBLIQUES --#
-# ----------------------- #
+    // On filtre la requête
+    $args   = array();
+    $filter = new FilterChainRoot(array(new ContextBuilderFilter(), 
+					new AuthenticateFilter()));
+    $filter->execute(&$args);
 
-    /** Constructeur. */
-    function Server_Atom () {}
+    // On construit le controlleur selon le type d'objet de la requête
+    $ctrlerFactory = new controllerFactory();
+    $ctrler        = $ctrlerFactory->createController($args['object']);
+
+    // On lance le controlleur pour l'objet de la requête
+    $response = $ctrler->execute($args);
     
-    
-# ----------------------- #
-# -- METHODES PRIVEES   --#
-# ----------------------- #
-    
-    
+    // On applique le renderer atom a la reponse et on la renvoit
+    $renderer = new atom_renderer();
+    return $renderer->render($response);
+  }
 }
-?>
 
+/* code principal du serveur */
+$server = new Server_Atom('http://localhost/blogmarks/atom/server');
+$server->run();
+?>

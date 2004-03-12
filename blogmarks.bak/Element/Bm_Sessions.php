9e6b5b04d6d76886336beec72567a60df7cdd3c2
mbertier <mbertier>
2004-03-12 16:55:40 +0000

    Regénération suite à une modification de la base de données

--- blogmarks.bak/Element/Bm_Sessions.php
+++ blogmarks.bak/Element/Bm_Sessions.php
@@ -6,13 +6,14 @@ require_once 'blogmarks/Element.php';
 
 class Element_Bm_Sessions extends Blogmarks_Element 
 {
-
     ###START_AUTOCODE
     /* the code below is auto generated do not remove the above tag */
 
     var $__table = 'bm_Sessions';                     // table name
-    var $id;                              // string(255)  not_null
+    var $id;                              // string(255)  not_null primary_key
+    var $user_id;                         // int(11)  not_null
     var $expire;                          // timestamp(14)  not_null unsigned zerofill timestamp
+    var $data;                            // blob(16777215)  not_null blob
 
     /* ZE2 compatibility trick*/
     function __clone() { return $this;}

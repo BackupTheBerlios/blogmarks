548a8f084b039fb17e6f7edb6df7ee90b99786f6
mbertier <mbertier>
2004-05-19 09:44:31 +0000

    MAJ suite à modification de la DB

--- Blogmarks/Blogmarks/Element/Bm_Sessions.php
+++ Blogmarks/Blogmarks/Element/Bm_Sessions.php
@@ -1,6 +1,6 @@
 <?php
 /** Table Definition for bm_Sessions
- * @version  $Id: Bm_Sessions.php,v 1.1 2004/03/31 14:57:45 mbertier Exp $
+ * @version  $Id: Bm_Sessions.php,v 1.2 2004/05/19 09:44:31 mbertier Exp $
  */
 
 /***/
@@ -17,8 +17,7 @@ class Element_Bm_Sessions extends Blogmarks_Element
 
     var $__table = 'bm_Sessions';                     // table name
     var $id;                              // string(255)  not_null primary_key
-    var $user_id;                         // int(11)  not_null
-    var $expire;                          // timestamp(14)  not_null unsigned zerofill timestamp
+    var $last_update;                     // timestamp(14)  not_null unsigned zerofill timestamp
     var $data;                            // blob(16777215)  not_null blob
 
     /* ZE2 compatibility trick*/

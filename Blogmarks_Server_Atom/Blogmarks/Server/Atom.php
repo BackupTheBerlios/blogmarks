76bbf251b931be80547162ff8b53d485200a7ceb
mbertier <mbertier>
2004-05-19 13:21:36 +0000

    Déménagement

---
+++ Blogmarks_Server_Atom/Blogmarks/Server/Atom.php
@@ -0,0 +1,92 @@
+<?php
+/** Déclaration de la classe Server_Atom
+ * @version    $Id: Atom.php,v 1.1 2004/05/19 13:21:36 mbertier Exp $
+ */
+ini_set( 'include_path', ini_get('include_path') . ':/home/benoit/dev/' );
+
+require_once 'PEAR.php';
+require_once 'Blogmarks/Blogmarks.php';
+require_once 'Blogmarks/Server/Atom/Filter.php';
+require_once 'Blogmarks/Server/Atom/Controller.php';
+require_once 'Blogmarks/Server/Atom/Renderer.php';
+
+/** Classe du serveur Atom de BlogMarks.
+ * @package    Servers
+ * @subpackage Atom
+ */
+class Server_Atom {
+
+  /** Constructeur. */
+  function Server_Atom () {
+  }
+
+  /** Lance le serveur */
+  function run () {
+
+    // racine du serveur a enregistrer qq part (pour l'instant ici)
+    $root = '/blogmarks/servers/atom';
+
+    // On construit le tableau d'arguments pour les filtres
+    $args   = array();
+    // on extrait l'URI relative
+    $uri = $_SERVER['REQUEST_URI'];
+
+    //***** DEBUG echo $uri."<br/>".$root;
+
+    $uri = ereg_replace($root, '', $uri);
+    $args['uri'] = $uri;
+    $args['method'] = $_SERVER['REQUEST_METHOD'];
+    $args['content'] = $_GLOBALS['HTTP_RAW_POST_DATA'];
+
+    // On filtre la requête
+    $filter = new FilterChainRoot(array(new ContextBuilderFilter(), 
+					new AuthenticateFilter()));
+    $ret = $filter->execute(&$args);
+    if ( BlogMarks::isError ($ret) ) {
+      
+      // erreur de filtre
+      echo $ret->getMessage();
+      return;
+    }
+
+    // **** DEBUG
+    echo "objet : ".$args['object']."<br/>";
+    echo "method : ".$args['method']."<br/>";
+    echo "tag : ".$args['tag']."<br/>";
+    echo "user : ".$args['user']."<br/>";
+    echo "id : ".$args['id']."<br/>";
+    echo "auth_str :".$args['auth_str']."<br/>";
+    // **********
+
+    // On construit le controlleur selon le type d'objet de la requête
+    $ctrlerFactory = new ControllerFactory();
+    $ctrler        = $ctrlerFactory->createController($args['object']);
+
+    if ( BlogMarks::isError ($ctrler) ) {
+      echo $ctrler->getMessage();
+      exit(1);
+    }
+
+    // On lance le controlleur pour l'objet de la requête
+    $response = $ctrler->execute($args);
+    if ( BlogMarks::isError($response) ) {
+      
+      // erreur du controlleur de requête
+      return;
+    }
+
+    // On applique le renderer atom a la reponse et on la renvoit
+    $rendererFactory = new rendererFactory();
+    $renderer = $rendererFactory->createRenderer($args['object']);
+
+    // GERER LA REPONSE HTTP
+    echo "HTTP/1.1 200 Ok\n";
+    $response->accept($renderer);
+    echo $renderer->render();
+  }
+}
+
+/* code principal du serveur */
+$server = new Server_Atom('http://localhost/blogmarks/atom/server');
+$server->run();
+?>
---
+++ Blogmarks_Server_Atom/Blogmarks/Server/Atom/Controller.php
@@ -0,0 +1,314 @@
+<?php
+/** Déclaration des différents controlleurs et de leur Factory.
+ * @version    $Id: Controller.php,v 1.1 2004/05/19 13:21:36 mbertier Exp $
+ */
+
+require_once '../Marker.php';
+
+/** Classe parente des différents controlleurs.
+ * @package    Atom
+ */
+class Controller {
+  
+  function Controller () {}
+  
+  function execute () {}
+}
+
+/** Factory de controlleurs.
+ * @package    Atom
+ */
+class ControllerFactory {
+
+  function createController($object)
+  {
+    $controller = null;
+
+    switch($object){
+
+    case ('Mark'):
+      $controller = new MarkController();
+      break;
+
+    case ('Tag'):
+      $controller = new TagController();
+      break;
+
+    case ('MarksList'):
+      $controller = new MarksListController();
+      break;
+
+    case ('TagsList'):
+      $controller = new TagsListController();
+      break;
+
+    default:
+      return BlogMarks::raiseError($object.' n\'est pas un objet blogmark', 
+				   500);
+    }
+    
+    return $controller;
+  }
+}
+
+
+####################################
+# Les différents controlleurs      #
+####################################
+
+
+
+/** Controlleur sur un Mark.
+ * @package    Atom
+ */
+class MarkController {
+
+  function execute ($args) {
+
+    $marker = new BlogMarks_marker;
+
+    switch ($args['method']) {
+
+    case 'GET':
+      // renvoit un mark
+      return $marker->getMark($args['id'], $args['user'], $args['auth_str']);
+
+    case 'PUT':
+      // met a jour un mark
+      $mark_array = $this->parseAtomMark($args['content']);
+      return $marker->updateMark($args['id'], $args['user'],
+				 $mark_array, $args['auth_str']);
+
+    case 'DELETE':
+      // supprimme un mark
+      return $marker->deleteMark($args['id'], $args['user'], 
+				 $args['auth_str']);
+
+    case 'POST':
+      // crée un mark
+      $mark_array = $this->parseAtomMark($args['content']);
+      return $marker->createMark($args['user'], $mark_array, 
+				 $args['auth_str']);     
+    }
+  }
+
+  /** Parse un document XML décrivant un Mark et renvoit un tableau de valeurs.
+   */
+  function parseAtom ($buffer) {
+
+    $props = array ();
+
+    // on construit le DOM à partir du flux XML
+    if ( !$dom = domxml_open_mem($buffer) )
+      return BlogMarks::raiseError('Impossible de créer le document DOM', 500);
+    
+    // récupère l'élément racine (blogmark)
+    $root = $dom->document_element();
+    
+    // enregistre la langue du mark
+    $props['lang'] = $root->get_attribute('lang');
+
+    // enregistre le href du link
+    $link = $root->get_elements_by_tagname('link');
+    $props['href'] = $link[0]->get_attribute('href');
+
+    // enregistre le nom de l'auteur du mark
+    $author = $root->get_elements_by_tagname('author');
+    $name = $author[0]->get_elements_by_tagname('name');
+    $props['login'] = $name[0]->get_content();
+
+    // enregistre le titre du mark
+    $title = $root->get_elements_by_tagname('title');
+    $props['title'] = $title[0]->get_content();
+
+    // enregistre la description du mark
+    $summary = $root->get_elements_by_tagname('summary');
+    $props['summary'] = $summary[0]->get_content();
+
+    // enregistre la date de publication
+    $issued = $root->get_elements_by_tagname('issued');
+    if ( !$issued = array() )
+      $props['issued'] = $issued[0]->get_content();
+
+    // enregistre le href de via
+    $via = $root->get_elements_by_tagname('via');
+    if ( $via != array() )
+      $props['via'] = $via[0]->get_attribute('href');   
+    
+    // enregistre le tableau de tags
+    $tags = $root->get_elements_by_tagname('tags');
+    if ( $tags != array() ) {
+
+      $props['tags'] = array();
+
+      $tags = $tags->get_elements_by_tagname('tag');
+
+      // on construit le tableau de chaque tag (juste title) 
+      // puis on l'empile dans le tableau principal
+      foreach ( $tags as $tag ) {
+
+	$tag_props = array();
+
+	// on récupère le nom du tag
+	$title = $tag->get_elements_by_tagname('title');
+	$tag_props['title'] = $title[0]->get_content();
+
+	// on empile
+	array_push($props['tags'], $tag_props);
+      }
+    }
+    
+    return $props;
+  }
+  
+}
+
+/** Controlleur sur un Tag.
+ * @package    Atom
+ */
+class TagController {
+
+  function execute ($args) {
+ 
+    $marker = new BlogMarks_marker;
+    
+    switch ($args['method']) {
+
+    case 'GET':
+
+      if ( isset($args['user']) )
+
+	// renvoit un tag privé
+	return $marker->getTag($args['tag'], $args['user'], $args['auth_str']);
+
+      else
+
+	// renvoit un tag publique
+	return $marker->getTag($args['tag']);
+
+    case 'PUT':
+
+      $tag_array = $this->parseAtomTag($args['content']);
+
+      if ( isset($args['user']) )
+
+	// met à jour un tag privé
+	return $marker->updateTag($args['tag'], $tag_array,
+				  $args['user'], $args['auth_str']);
+
+      else
+
+	// met à jour un tag publique
+	return $marker->updateTag($args['tag'], $tag_array);     
+
+    case 'DELETE':
+
+      if ( isset($args['user']) )
+
+	// supprimme un tag privé
+	return $marker->deleteTag($args['tag'], $args['user'], 
+				  $args['auth_str']);
+
+      else
+
+	// supprimme un tag publique
+	return $marker->deleteTag($args['tag']);	
+
+    case 'POST':
+
+      $tag_array = $this->parseAtomTag($args['content']);
+
+      if ( isset($args['user']) )
+
+	// crée un tag privé
+    	return $marker->createTag($args['tag'], $tag_array,
+				  $args['user'], $args['auth_str']);
+
+      else
+
+	// crée un tag publique
+	return $marker->createTag($args['tag'], $tag_array);    
+    }
+  }
+
+  /** Parse un document XML décrivant un Tag et renvoit un tableau de valeurs.
+   */
+  function parseAtom ($buffer) {
+
+    $props = array ();
+
+    // on construit le DOM à partir du flux XML
+    if ( !$dom = domxml_open_mem($buffer) )
+      return BlogMarks::raiseError('Impossible de créer le document DOM', 500);
+    
+    // récupère l'élément racine (tag)
+    $root = $dom->document_element();
+    
+    // enregistre la langue du tag
+    $props['lang'] = $root->get_attribute('lang');
+
+    // enregistre le titre du tag
+    $title = $root->get_elements_by_tagname('title');
+    $props['title'] = $title[0]->get_content();
+
+    // enregistre la description du tag
+    $summary = $root->get_elements_by_tagname('summary');
+    $props['summary'] = $summary[0]->get_content();
+
+    // enregistre le nom du tag parent
+    $subTagOf = $root->get_elements_by_tagname('subTagOf');
+    if ( !$subTagOf = array() )
+      $props['subTagOf'] = $subTagOf[0]->get_content();
+    
+    return $props;
+  } 
+}
+
+/** Controlleur sur une liste de Marks.
+ * @package    Atom
+ */
+class MarksListController {
+
+  function execute ($args) {
+
+    $marker = new BlogMarks_marker;
+
+    if ( isset($args['user']) ) {
+
+      if ( isset($args['tag']) )
+
+	// liste des Marks d'un utilisateur avec un tag
+	return $marker->getMarksListOfUserOfTag($args['user'], $args['tag'],
+						$args['auth_str']);
+      
+      else
+
+	// liste des Marks d'un utilisateur
+	return $marker->getMarksListOfUser($args['user'], $args['auth_str']);
+    }
+    
+    // sinon c'est une liste de tags publics
+    return $marker->getMarksListOfTag($args['tag'], $args['auth_str']);
+  }
+}
+
+/** Controlleur sur une liste de Tags.
+ * @package    Atom
+ */
+class TagsListController {
+
+  function execute ($args) {
+
+    $marker = new BlogMarks_marker;
+
+    if ( isset($args['user']) )
+
+      // liste de tags privés
+      return $marker->getTagsListOfTag($args['tag']);
+
+    else
+
+      // liste de tags publiques
+      return $marker->getTagsListOfUserOfTag($args['tag']);
+  }
+}
